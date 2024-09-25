e024117f027aa2df26f0f359e0e851ca8e8b839c
Marcelo Hosan <mhosan@gmail.com>
2024-09-25 20:08:25 -0300

    armando la toolbar

--- src/app/components/toolbar/toolbar.component.ts
+++ src/app/components/toolbar/toolbar.component.ts
@@ -1,13 +1,28 @@
 import { Component } from '@angular/core';
+import { MatIconModule} from '@angular/material/icon';
+import { MatToolbarModule} from '@angular/material/toolbar';
+import { MatButtonModule} from '@angular/material/button';
+
+const MATERIAL_MODULES = [MatToolbarModule, MatIconModule, MatButtonModule]
 
 @Component({
   selector: 'app-toolbar',
   standalone: true,
-  imports: [],
+  imports: [MATERIAL_MODULES],
   template: `
-    <p>
-      toolbar works!
-    </p>
+    <mat-toolbar color="primary">
+      <a mat-button routerLink="/">
+        <mat-icon>home</mat-icon>
+        <span>home</span></a>
+
+      <a mat-button routerLink="/contacts">
+        <mat-icon>list_alt</mat-icon>
+        <span>Contacts</span></a>
+
+      <a mat-button >
+        <mat-icon>add_box</mat-icon>
+        <span>New</span></a>
+    </mat-toolbar>
   `,
   styles: ``
 })
