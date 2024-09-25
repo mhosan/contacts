8dfd35e45319cc7433272142b0b1c259c1ad3c67
mhosan <mhosan@gmail.com>
2024-09-25 15:24:47 -0300

    testeando rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,5 +2,6 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
     {path: '', redirectTo:'/contacts', pathMatch: 'full' },
-    {path: 'contacts', loadChildren: ()=> import('./features/contacts/contacts.routes')}
+    {path: 'contacts', loadChildren: ()=> import('./features/contacts/contacts.routes')},
+    {path: '**', redirectTo: '/contacts'}
 ];
