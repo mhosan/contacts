92472226843854042b8b92eec4f9e8a5a70694d0
Marcelo Hosan <mhosan@gmail.com>
2024-09-28 20:08:46 -0300

    armando toolbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ToolbarComponent } from '@components/toolbar/toolbar.component';
+import { MatCardModule } from '@angular/material/card';
+
+const MATERIAL_MODULES =[MatCardModule]
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ToolbarComponent],
+  imports: [RouterOutlet, ToolbarComponent, MATERIAL_MODULES],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'contact';
+
+  onClickNewContact():void{}
 }
--- src/app/components/toolbar/toolbar.component.ts
+++ src/app/components/toolbar/toolbar.component.ts
@@ -1,7 +1,7 @@
-import { Component } from '@angular/core';
-import { MatIconModule} from '@angular/material/icon';
-import { MatToolbarModule} from '@angular/material/toolbar';
-import { MatButtonModule} from '@angular/material/button';
+import { Component, output } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
 
 const MATERIAL_MODULES = [MatToolbarModule, MatIconModule, MatButtonModule]
 
@@ -13,19 +13,27 @@ const MATERIAL_MODULES = [MatToolbarModule, MatIconModule, MatButtonModule]
     <mat-toolbar color="primary">
       <a mat-button routerLink="/">
         <mat-icon>home</mat-icon>
-        <span>home</span></a>
+        <span>home</span>
+      </a>
 
       <a mat-button routerLink="/contacts">
         <mat-icon>list_alt</mat-icon>
         <span>Contacts</span></a>
-
-      <a mat-button >
+      
+      <span class="spacer"></span>
+      <a mat-button (click)="emitClick()">
         <mat-icon>add_box</mat-icon>
-        <span>New</span></a>
+        <span>New</span>
+      </a>
+
     </mat-toolbar>
   `,
   styles: ``
 })
 export class ToolbarComponent {
-
+  onNewContactEvent = output<void>();
+  
+  emitClick(): void{
+    this.onNewContactEvent.emit();
+  }
 }
