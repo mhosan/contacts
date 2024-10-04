7f553564e5a20c9ba2e788afcb55a280e88469d4
Marcelo Hosan <mhosan@gmail.com>
2024-10-04 11:36:07 -0300

    el componente tabla ordena bien

--- src/app/components/grid/grid.component.ts
+++ src/app/components/grid/grid.component.ts
@@ -1,5 +1,6 @@
-import { Component, input, OnInit } from '@angular/core';
+import { Component, input, OnInit, viewChild } from '@angular/core';
 import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { MatSort, MatSortModule } from '@angular/material/sort';
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 
 
@@ -7,7 +8,7 @@ import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 @Component({
   selector: 'app-grid',
   standalone: true,
-  imports: [MatTableModule, MatPaginatorModule],
+  imports: [MatTableModule, MatPaginatorModule, MatSortModule],
   templateUrl: './grid.component.html',
   styleUrl: './grid.component.scss'
 })
@@ -19,9 +20,11 @@ export class GridComponent<T> implements OnInit {
   displayedColumns = input.required<string[]>();  //recibo las columnas desde fuera
   data = input.required<T[]>()                    //recibo la data desde fuera
   dataSource = new MatTableDataSource<T>();
+  private readonly _sort = viewChild.required<MatSort>(MatSort);
 
   ngOnInit(): void {
-    this.dataSource.data=this.data();
+    this.dataSource.data = this.data();
+    this.dataSource.sort = this._sort();
   }
-  
+
 }
