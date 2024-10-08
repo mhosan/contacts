49442983e745bbe6aebc05fd1fbba4acb06a9979
mhosan <mhosan@gmail.com>
2024-10-08 17:31:44 -0300

    ajustando el filtro para que funcione con la signal

--- src/app/components/grid/grid.component.ts
+++ src/app/components/grid/grid.component.ts
@@ -1,4 +1,4 @@
-import { Component, input, OnInit, signal, viewChild } from '@angular/core';
+import { Component, effect, input, OnInit, signal, viewChild } from '@angular/core';
 import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
 import { MatSort, MatSortModule } from '@angular/material/sort';
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
@@ -14,24 +14,26 @@ import { FilterComponent } from './filter/filter.component';
   styleUrl: './grid.component.scss'
 })
 export class GridComponent<T> implements OnInit {
-  /* displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
-  dataSource = ELEMENT_DATA;
- */
-
   displayedColumns = input.required<string[]>();  //recibo las columnas desde fuera
   data = input.required<T[]>()                    //recibo la data desde fuera
   dataSource = new MatTableDataSource<T>();
   valueToFilter = signal('');
   private readonly _sort = viewChild.required<MatSort>(MatSort);
   private readonly _paginator = viewChild.required<MatPaginator>(MatPaginator);
-
+  constructor() {
+    effect(()=>{
+      if(this.valueToFilter()){
+        this.dataSource.filter = this.valueToFilter();
+      }
+    }, {allowSignalWrites: true});//allowSignalWrites: true permite modificar la signal dentro del effect, cosa que no está permitida por default.
+  }
   ngOnInit(): void {
     this.dataSource.data = this.data();
     this.dataSource.sort = this._sort();
-    this.dataSource.paginator= this._paginator();
+    this.dataSource.paginator = this._paginator();
   }
 
-  applyFilter(event:Event):void{
+  applyFilter(event: Event): void {
     const filterValue = (event.target as HTMLInputElement).value;
     this.dataSource.filter = filterValue.trim().toLowerCase()
 
